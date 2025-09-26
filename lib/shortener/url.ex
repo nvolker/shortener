e9790ed3725b17a380432c18c6bc31f7e4c8b6bd
Nicholas Volker Moorcroft <nvolker@users.noreply.github.com>
2025-09-27 01:18:32 +0200

    Refactor URL length constraints to use module attributes for maintainability

--- lib/shortener/url.ex
+++ lib/shortener/url.ex
@@ -2,6 +2,10 @@ defmodule Shortener.Url do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # Field length constraints - must match database migration constraints
+  @max_original_url_length 2048  # Supports very long URLs including query parameters
+  @max_slug_length 255           # Reasonable limit for short, memorable identifiers
+
   schema "urls" do
     field :original_url, :string
     field :slug, :string
@@ -15,8 +19,8 @@ defmodule Shortener.Url do
     url
     |> cast(attrs, [:original_url, :slug, :click_count])
     |> validate_required([:original_url, :slug])
-    |> validate_length(:original_url, max: 2048)
-    |> validate_length(:slug, max: 255)
+    |> validate_length(:original_url, max: @max_original_url_length)
+    |> validate_length(:slug, max: @max_slug_length)
     |> unique_constraint(:slug)
   end
 end
